493caef28dbadf18835028e62fcc43f63d376cd6
darrjone <darren@morninglory.com>
2018-12-10 19:10:07 +0100

    finalising

--- tests/AppBundle/Service/RestaurantServiceTest.php
+++ tests/AppBundle/Service/RestaurantServiceTest.php
@@ -25,10 +25,11 @@ class RestaurantServiceTest extends WebTestCase
         $this->restaurantService = $restaurantService;
 
         $restaurants = [];
+        $statuses = ["open", "order ahead", "closed"];
         for($i = 0; $i < 5; $i++){
             $restaurants[] = new RestaurantModel([
                 "name" => "test{$i}",
-                "status" => array_rand(["open", "open ahead", "close"])[0],
+                "status" => $statuses[array_rand($statuses, 1)],
                 "sortingValues" => [
                     "bestMatch" => random_int(0, 100),
                     "newest" => random_int(0, 100),
@@ -52,28 +53,28 @@ class RestaurantServiceTest extends WebTestCase
      * Doing a thorough test in the set Favourite by key
      * @throws \AppBundle\Exception\InvalidFavouriteUidException
      */
-    public function testSetFavouriteByKey()
+    public function testSetFavouriteByUid()
     {
         /**
          * @var RestaurantModel $restaurant
          */
         $restaurant = $this->restaurants[0];
 
-        //should return the value that was given
+        //should return the value that was given (true)
         $result = $this->restaurantService->setFavouriteByUid(
             $restaurant->getUid(), true
         );
-
         $this->assertEquals(true, $result);
 
         //should contain a key name favourite once you add a favoured restaurant
         $result = $this->restaurantService->getAllGrouped();
-        $this->assertEquals(true, array_key_exists("favourite", $result));
+        $this->assertEquals(true, array_key_exists("favourites", $result));
 
+
+        //should return the value give (false)
         $result = $this->restaurantService->setFavouriteByUid(
             $restaurant->getUid(), false
         );
-
         $this->assertEquals(false, $result);
 
     }
